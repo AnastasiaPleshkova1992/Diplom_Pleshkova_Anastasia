4d0fa795eb71070a83bb527a8c82cf6176ca66db
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-08-15 14:27:57 +0500

    correct types in auth routers

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -9,13 +9,17 @@ from src.users.schemas import CurrentUserResponseModel
 router = APIRouter(tags=["Auth"])
 
 
-@router.post("/login",
-             summary="Вход в систему",
-             response_model=CurrentUserResponseModel,
-             status_code=200)
-async def login(response: Response,
-                user_data: LoginModel,
-                session: AsyncSession = Depends(db_helper.session_getter)):
+@router.post(
+    "/login",
+    summary="Вход в систему",
+    response_model=CurrentUserResponseModel,
+    status_code=200,
+)
+async def login(
+    response: Response,
+    user_data: LoginModel,
+    session: AsyncSession = Depends(db_helper.session_getter),
+) -> CurrentUserResponseModel:
     """После успешного входа в систему необходимо установить Cookies для пользователя"""
     check = await authenticate_user(
         session=session, login=user_data.login, password=user_data.password
