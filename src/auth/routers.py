f600b488b03a929877f904f3bd1ac0c482b2c41b
PleshkovaAnastasia1992 <nastya.tsvetkova@rambler.ru>
2024-08-15 13:33:07 +0500

    correct routers for authentication

--- src/auth/routers.py
+++ src/auth/routers.py
@@ -9,14 +9,13 @@ from src.users.schemas import CurrentUserResponseModel
 router = APIRouter(tags=["Auth"])
 
 
-@router.post(
-    "/login", summary="Вход в систему", response_model=CurrentUserResponseModel
-)
-async def auth_user(
-    response: Response,
-    user_data: LoginModel,
-    session: AsyncSession = Depends(db_helper.session_getter),
-):
+@router.post("/login",
+             summary="Вход в систему",
+             response_model=CurrentUserResponseModel,
+             status_code=200)
+async def login(response: Response,
+                user_data: LoginModel,
+                session: AsyncSession = Depends(db_helper.session_getter)):
     """После успешного входа в систему необходимо установить Cookies для пользователя"""
     check = await authenticate_user(
         session=session, login=user_data.login, password=user_data.password
@@ -30,7 +29,7 @@ async def auth_user(
     return check
 
 
-@router.post("/logout")
-async def logout_user(response: Response):
+@router.post("/logout", status_code=200)
+async def logout(response: Response):
     response.delete_cookie(key="users_access_token")
     return {"message": "Пользователь успешно вышел из системы"}
